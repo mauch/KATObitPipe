b5a6248571c7c468817105cf348e573fa8233f92
Tom Mauch <tmauch@ska.ac.za>
2023-06-15 08:54:36 +0000

    Select compscans=Track to avoid ref pointing scans.

--- katim/AIPSSetup.py
+++ katim/AIPSSetup.py
@@ -30,7 +30,7 @@ def AIPSSetup(err,configfile=None,scratchdir=None,overwrite=True,aipsdisk='aipsd
         aips_version = os.environ['AIPS_VERSION'][-7:]
     except:
         aips_dir = './'
-        aips_version = '31DEC23'    # Should sort out where to change this if necessary!!
+        aips_version = '31DEC22'    # Should sort out where to change this if necessary!!
 
     configdefaults   = {'aips_dir': aips_dir, 'obit_dir': OBIT_EXEC, 'aips_version': aips_version, 'scratch_area': cwd, 'metadata_dir': OBIT_DATA}
     config = configparser.ConfigParser(configdefaults)
