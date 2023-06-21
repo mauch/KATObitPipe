c9ce2567747124d1e0a70801f3ba873c28a766e1
Tom Mauch <tmauch@ska.ac.za>
2023-06-21 14:20:49 +0000

    Update Obit install defaults script.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 export OBIT_ROOT=/scratch2/tmauch/refpointingJune23/obit-distro-1.1.648
 export OBIT=${OBIT_ROOT}/lib/obit
 export OBIT_EXEC=${OBIT}
-export PATH=${PATH}:${OBIT}/bin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${OBIT}/lib
-export PYTHONPATH=${PYTHONPATH}:${OBIT}/share/obittalk/python:${OBIT}/share/python
+export PATH=${PATH}:${OBIT_ROOT}/bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${OBIT_ROOT}/lib
+export PYTHONPATH=${PYTHONPATH}:${OBIT_ROOT}/share/obittalk/python:${OBIT_ROOT}/share/python
